d615e09fa9ff34d9c3e1c0f9a41d7adebef71a64
huangyan321 <1604549268@qq.com>
2021-09-07 00:12:46 +0800

    添加uni依赖包

---
+++ pages/notepad/form/addForm.vue
@@ -0,0 +1,42 @@
+<template>
+	<view style="height: 100%; width: 100%;">
+		<view class="example-body">
+			<uni-datetime-picker v-model="single" :border="false" type="date"/>
+		</view>
+		<uni-card is-full="true">
+			<view>
+				<view class="uni-textarea">
+					<textarea  placeholder="请输入..." />
+				</view>
+			</view>
+		</uni-card>
+		 <button type="primary" plain="true" loading="true" style="margin-top: 80rpx;">保存</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				single: '2021-04-3',
+				pattern: {
+					buttonColor: "pink"
+				},
+				horizontal: "right",
+				vertical: "bottom",
+				direction: "vertical",
+
+			}
+		},
+		methods: {
+			fabClick() {
+				uni.navigateTo({
+					url: "form/addForm"
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
--- pages/notepad/index.vue
+++ pages/notepad/index.vue
@@ -1,7 +1,52 @@
 <template>
+	<view style="height: 100%; width: 100%;">
+		<view class="example-body">
+			<uni-datetime-picker v-model="range" type="daterange" rangeSeparator="至" />
+		</view>
+		<scroll-view>
+			<!-- 标题卡片模式 -->
+			<uni-card title="2021年9月5日" mode="title" :is-shadow="true" note="Tips">
+				阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴
+			</uni-card>
+			<uni-card title="2021年9月6日" mode="title" :is-shadow="true" note="Tips">
+				阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴
+			</uni-card>
+			<uni-card title="xxxx年x月x日" mode="title" :is-shadow="true" note="Tips">
+				阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴
+			</uni-card>
+			<uni-card title="xxxx年xx月x日" mode="title" :is-shadow="true" note="Tips">
+				阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴阿巴
+			</uni-card>
+		</scroll-view>
+		<view>
+			<uni-fab :pattern="pattern" :horizontal="horizontal" :vertical="vertical" :direction="direction"
+				@fabClick="fabClick" :popMenu="false"></uni-fab>
+		</view>
+	</view>
 </template>
 
 <script>
+	export default {
+		data() {
+			return {
+				range: ['2021-03-8', '2021-4-20'],
+				pattern: {
+					buttonColor: "pink"
+				},
+				horizontal: "right",
+				vertical: "bottom",
+				direction: "vertical",
+
+			}
+		},
+		methods: {
+			fabClick() {
+				uni.navigateTo({
+					url: "form/addForm"
+				})
+			}
+		}
+	}
 </script>
 
 <style>
